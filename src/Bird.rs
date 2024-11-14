ac3ec21ae2a8dec51d8a370e7977db01c7dcd496
疯不皮 <2921744480@qq.com>
2024-11-14 23:06:58 +0800

    feat: 小鸟移动完成

--- src/Bird.rs
+++ src/Bird.rs
@@ -1,16 +1,19 @@
-use std::time::{SystemTime, UNIX_EPOCH};
+// use std::cell::Cell;
+use std::cell::RefCell;
+use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 use web_sys::*;
 
-struct Bird {
+// Bird 结构体
+pub struct Bird {
     ctx: CanvasRenderingContext2d,
     bird_img: HtmlImageElement,
-    pub x: f64,
-    pub y: f64,
+    x: f64,
+    y: f64,
     bird_width: f64,
     bird_height: f64,
     index: f64,
-    v0: f64,
+    v0: Rc<RefCell<f64>>,
     acc: f64,
     start_time: f64,
     max_speed: f64,
@@ -18,77 +21,108 @@ struct Bird {
 }
 
 impl Bird {
-    fn new(ctx: CanvasRenderingContext2d, bird_img: HtmlImageElement) -> Bird {
-        let bird_width = bird_img.width() as f64 / 2.0;
-        let bird_height = bird_img.height() as f64 / 2.0;
-        let start_time = SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .unwrap()
-            .as_secs_f64();
+    // 创建一个新的 Bird
+    pub fn new(ctx: &CanvasRenderingContext2d, bird_img: HtmlImageElement) -> Bird {
+        let bird_width = bird_img.width() as f64 / 3.0;
+        let bird_height = bird_img.height() as f64;
+        let start_time = window().unwrap().performance().unwrap().now(); // 返回当前时间的高精度时间戳
+
         let mut bird = Bird {
-            ctx,
+            ctx: ctx.clone(),
             bird_img,
-            x: 100.0,
+            x: 50.0,
             y: 100.0,
             bird_width,
             bird_height,
             index: 0.0,
-            v0: 0.0,
+            v0: Rc::new(RefCell::new(0.0)),
             acc: 0.0005,
             start_time,
             max_speed: 0.3,
-            max_angle: 3.1415926 / 4.0,
+            max_angle: std::f64::consts::PI / 4.0,
         };
+
         bird.init_fly();
         bird
     }
 
+    // 初始化飞行，绑定点击事件
     fn init_fly(&mut self) {
-        let ctx = self.ctx.clone();
-        let bird = self.clone();
-        self.ctx.canvas().unwrap().set_onclick(Some(
-            &Closure::wrap(Box::new(move |_| {
-                bird.init_fly();
-            }) as Box<dyn FnMut(_)>)
-            .into_js_value(),
-        ));
+        let v0 = Rc::clone(&self.v0);
+        let cb = Closure::wrap(Box::new(move || {
+            *v0.borrow_mut() = -0.3;
+            log::info!("点击了鸟,v0:{}", *v0.borrow());
+        }) as Box<dyn FnMut()>);
+
+        // 绑定点击事件监听
+        self.ctx
+            .canvas()
+            .unwrap()
+            .add_event_listener_with_callback("click", cb.as_ref().unchecked_ref())
+            .unwrap();
+        cb.forget(); // 防止闭包被清理
     }
 
-    fn draw(&mut self) {
+    // 绘制鸟
+    pub fn draw(&mut self) {
         self.ctx.save();
-        self.ctx.translate(self.x, self.y);
-        let current_time = SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .unwrap()
-            .as_secs_f64();
-        let delta_time = current_time - self.start_time;
+        self.ctx.translate(self.x, self.y).unwrap(); // 控制小鸟位移
+
+        let current_time = window().unwrap().performance().unwrap().now(); // 返回当前时间的高精度时间戳
+        let delta_time = (current_time - self.start_time).floor();
         self.start_time = current_time;
-        let h = self.v0 * delta_time + self.acc * delta_time * delta_time / 2.0;
+
+        let h = *self.v0.borrow() * delta_time + self.acc * delta_time * delta_time / 2.0;
         self.y += h;
-        self.v0 += self.acc * delta_time;
-        let angle = self.v0 / self.max_speed * self.max_angle;
+
+        let v0 = Rc::new(RefCell::new(*self.v0.borrow() + self.acc * delta_time));
+        self.v0 = v0;
+
+        let angle = *self.v0.borrow() / self.max_speed * self.max_angle;
         if angle > self.max_angle {
             self.ctx.rotate(self.max_angle);
         } else {
             self.ctx.rotate(angle);
         }
+        // 绘制鸟的图像
         self.ctx
             .draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
                 &self.bird_img,
-                (self.index / 5.0).floor() * self.bird_width,
+                ((self.index / 5.0).floor() * self.bird_width).floor(),
                 0.0,
-                self.bird_width,
-                self.bird_height,
-                -self.bird_width / 2.0,
-                -self.bird_height / 2.0,
-                self.bird_width,
-                self.bird_height,
+                self.bird_width.floor(),
+                self.bird_height.floor(),
+                -(self.bird_width / 2.0).floor(),
+                -(self.bird_height / 2.0).floor(),
+                self.bird_width.floor(),
+                self.bird_height.floor(),
             )
             .unwrap();
+        // log::info!("当前self.v0: {}", *self.v0.borrow());
         self.index += 1.0;
         if self.index > 14.0 {
             self.index = 0.0;
         }
+
         self.ctx.restore();
+    }
+}
+
+impl Clone for Bird {
+    fn clone(&self) -> Self {
+        Bird {
+            ctx: self.ctx.clone(),
+            bird_img: self.bird_img.clone(),
+            x: self.x,
+            y: self.y,
+            bird_width: self.bird_width,
+            bird_height: self.bird_height,
+            index: self.index,
+            v0: self.v0.clone(),
+            acc: self.acc,
+            start_time: self.start_time,
+            max_speed: self.max_speed,
+            max_angle: self.max_angle,
+        }
     }
 }
