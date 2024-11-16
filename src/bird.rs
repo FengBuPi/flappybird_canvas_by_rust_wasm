19f4e05cef6e0ee9d7495365d322264930b2a422
疯不皮 <2921744480@qq.com>
2024-11-16 12:56:18 +0800

    style: 优化代码格式

--- src/bird.rs
+++ src/bird.rs
@@ -4,7 +4,6 @@ use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 use web_sys::*;
 
-// Bird 结构体
 pub struct Bird {
     ctx: CanvasRenderingContext2d,
     bird_img: HtmlImageElement,
@@ -18,53 +17,6 @@ pub struct Bird {
     start_time: f64,
     max_speed: f64,
     max_angle: f64,
-}
-
-impl Draw for Bird {
-    // 绘制鸟
-    fn draw(&mut self) {
-        self.ctx.save();
-        self.ctx.translate(self.x, self.y).unwrap(); // 控制小鸟位移
-
-        let current_time = window().unwrap().performance().unwrap().now(); // 返回当前时间的高精度时间戳
-        let delta_time = (current_time - self.start_time).floor();
-        self.start_time = current_time;
-
-        // 小鸟位移
-        self.y += *self.v0.borrow() * delta_time + self.acc * delta_time * delta_time / 2.0;
-
-        let current_v0 = *self.v0.borrow(); // 不可变借用一次，缓存当前值
-        *self.v0.borrow_mut() = current_v0 + self.acc * delta_time;
-
-        // 小鸟旋转
-        let angle = *self.v0.borrow() / self.max_speed * self.max_angle;
-        if angle > self.max_angle {
-            self.ctx.rotate(self.max_angle);
-        } else {
-            self.ctx.rotate(angle);
-        }
-        // 绘制鸟的图像
-        self.ctx
-            .draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
-                &self.bird_img,
-                ((self.index / 5.0).floor() * self.bird_width).floor(),
-                0.0,
-                self.bird_width.floor(),
-                self.bird_height.floor(),
-                -(self.bird_width / 2.0).floor(),
-                -(self.bird_height / 2.0).floor(),
-                self.bird_width.floor(),
-                self.bird_height.floor(),
-            )
-            .unwrap();
-        // log::info!("当前self.v0: {}", *self.v0.borrow());
-        self.index += 1.0;
-        if self.index > 14.0 {
-            self.index = 0.0;
-        }
-
-        self.ctx.restore();
-    }
 }
 
 impl Bird {
@@ -111,21 +63,49 @@ impl Bird {
     }
 }
 
-// impl Clone for Bird {
-//     fn clone(&self) -> Self {
-//         Bird {
-//             ctx: self.ctx.clone(),
-//             bird_img: self.bird_img.clone(),
-//             x: self.x,
-//             y: self.y,
-//             bird_width: self.bird_width,
-//             bird_height: self.bird_height,
-//             index: self.index,
-//             v0: self.v0.clone(),
-//             acc: self.acc,
-//             start_time: self.start_time,
-//             max_speed: self.max_speed,
-//             max_angle: self.max_angle,
-//         }
-//     }
-// }
+impl Draw for Bird {
+    // 绘制鸟
+    fn draw(&mut self) {
+        self.ctx.save();
+        self.ctx.translate(self.x, self.y).unwrap(); // 控制小鸟位移
+
+        let current_time = window().unwrap().performance().unwrap().now(); // 返回当前时间的高精度时间戳
+        let delta_time = (current_time - self.start_time).floor();
+        self.start_time = current_time;
+
+        // 小鸟位移
+        self.y += *self.v0.borrow() * delta_time + self.acc * delta_time * delta_time / 2.0;
+
+        let current_v0 = *self.v0.borrow(); // 不可变借用一次，缓存当前值
+        *self.v0.borrow_mut() = current_v0 + self.acc * delta_time;
+
+        // 小鸟旋转
+        let angle = *self.v0.borrow() / self.max_speed * self.max_angle;
+        if angle > self.max_angle {
+            self.ctx.rotate(self.max_angle).unwrap();
+        } else {
+            self.ctx.rotate(angle).unwrap();
+        }
+        // 绘制鸟的图像
+        self.ctx
+            .draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+                &self.bird_img,
+                ((self.index / 5.0).floor() * self.bird_width).floor(),
+                0.0,
+                self.bird_width.floor(),
+                self.bird_height.floor(),
+                -(self.bird_width / 2.0).floor(),
+                -(self.bird_height / 2.0).floor(),
+                self.bird_width.floor(),
+                self.bird_height.floor(),
+            )
+            .unwrap();
+        // log::info!("当前self.v0: {}", *self.v0.borrow());
+        self.index += 1.0;
+        if self.index > 14.0 {
+            self.index = 0.0;
+        }
+
+        self.ctx.restore();
+    }
+}
