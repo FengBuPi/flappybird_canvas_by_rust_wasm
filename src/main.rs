312753fab6f122e0a3212c805b83a08a475f57b2
疯不皮 <2921744480@qq.com>
2024-11-16 12:56:56 +0800

    feat: 删除main文件夹

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use std::time::{SystemTime, UNIX_EPOCH};
-
-fn main() {
-    let time = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_millis();
-    println!("{}", time);
-}
