6af652d24f4b5e1b15e605a97c311be96a327f88
疯不皮 <2921744480@qq.com>
2024-11-16 12:17:01 +0800

    feat: 文件模块化声明

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
-mod Bird;
-mod Game;
-// mod Land;
-// mod Pipe;
-// mod Sky;
+// Draw Trait
+pub mod draw;
+
+// Game Module
+pub mod bird;
+pub mod game;
+pub mod land;
+pub mod pipe;
+pub mod sky;
