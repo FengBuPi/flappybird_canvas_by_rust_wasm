5293ecb8f8df5b694b89eeaa2dfb4ecf7b632224
疯不皮 <2921744480@qq.com>
2025-03-02 20:23:37 +0800

    feat: 规范代码样式

--- src_ts/components/gameContainer.ts
+++ src_ts/components/gameContainer.ts
@@ -1,36 +1,38 @@
-import init, { Game } from '../../pkg/flappybird_canvas_by_rust_wasm.js';
-import runing from '../State/gameState.js';
-import { Component } from './../utils/Router';
-import { initGamePauseModule } from '../components/gamePause.js';
+import init, { Game } from "../../pkg/flappybird_canvas_by_rust_wasm.js";
+import runing from "../State/gameState.js";
+import { Component } from "./../utils/Router";
+import { initGamePauseModule } from "../components/gamePause.js";
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 let raf: number = 0;
 async function startGame() {
   await init();
-  const game = new Game('canvas');
+  const game = new Game("canvas");
   game.game_init();
-  game.frame()
+  game.frame();
   function animation() {
     if (runing.value) {
-      game.frame()
+      game.frame();
     }
     raf = requestAnimationFrame(animation);
   }
-  animation()
+  animation();
 }
 
 function mounted() {
-  initGamePauseModule.mounted()
-  startGame()
+  initGamePauseModule.mounted();
+  startGame();
 }
 
 function unmounted() {
-  initGamePauseModule.unmounted()
-  cancelAnimationFrame(raf)
+  initGamePauseModule.unmounted();
+  cancelAnimationFrame(raf);
 }
 const initGameContainerModule: Component = {
-  template: document.getElementById('game-container')!,
+  template: document.getElementById("game-container")!,
   mounted,
   unmounted,
-}
+};
 
-export { initGameContainerModule }
+export { initGameContainerModule };
